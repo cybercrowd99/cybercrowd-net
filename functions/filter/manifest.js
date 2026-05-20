fe58e8765c7618c56a0c27549bf66a59d1efec47
Cybercrowd <cybercrowd@yahoo.com>
2026-05-20 12:36:09 -0500

    Add filter-manifest endpoint enumerating CyberCrowd request-filter surfaces
    Introduces the CyberCrowd filter-manifest endpoint that enumerates all filter 
    surfaces including static filters, dynamic filters, sanitation filters, and 
    normalization filters. Begins the Platform Filter Layer following completion 
    of the Rule Layer.

--- functions/filter/manifest.js
+++ functions/filter/manifest.js
@@ -1,1 +1,27 @@
 export async function onRequest() {
+  const manifest = {
+    staticFilters: [
+      "strip_null_bytes",
+      "enforce_utf8",
+      "trim_whitespace"
+    ],
+    dynamicFilters: [
+      "adaptive_sanitization",
+      "rate_sensitive_filtering"
+    ],
+    sanitationFilters: [
+      "remove_control_chars",
+      "collapse_repeated_delimiters"
+    ],
+    normalizationFilters: [
+      "unicode_normalize",
+      "header_normalize"
+    ]
+  };
+
+  return new Response(JSON.stringify({
+    manifest
+  }, null, 2), {
+    headers: { "Content-Type": "application/json" }
+  });
+}
