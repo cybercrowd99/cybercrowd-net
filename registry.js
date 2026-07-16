e0be760b9336d91eaab3f3f20d1c598935f656df
Cybercrowd <cybercrowd@yahoo.com>
2026-07-16 00:41:11 -0500

    Add NET_ROUTES for local event score route

---
+++ registry.js
@@ -0,0 +1,8 @@
+// net/registry.js
+
+import LocalEventScoreRoute from './local-event-score.route';
+
+export const NET_ROUTES = {
+  LESScoreMoment: LocalEventScoreRoute,
+  // existing routes stay untouched
+};
