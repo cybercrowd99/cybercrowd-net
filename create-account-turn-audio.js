97d9c889cbc91aa9a2bf663b10f5c03f3f5ad866
Cybercrowd <cybercrowd@yahoo.com>
2026-08-24 01:17:06 -0500

    Fix URL syntax for TURN_AUDIO_URL

--- create-account-turn-audio.js
+++ create-account-turn-audio.js
@@ -20,7 +20,7 @@
 // sound-effects/Surface-closing_1sEffect.mp3
 
 const TURN_AUDIO_URL =
-https://b15830fa5a72b2e179236cb740dd762d.r2.cloudflarestorage.com/sound-effects/Surface-closing_1sEffect.mp3";
+"https://b15830fa5a72b2e179236cb740dd762d.r2.cloudflarestorage.com/sound-effects/Surface-closing_1sEffect.mp3";
 
 const turnAudio =
   new Audio(TURN_AUDIO_URL);
